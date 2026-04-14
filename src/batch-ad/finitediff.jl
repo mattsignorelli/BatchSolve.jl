5fba56a84f81c3863df0f4e11ab968c6191cede5
mattsignorelli <mgs255@cornell.edu>
2026-04-14 10:42:09 -0400

    amost done

--- src/batch-ad/finitediff.jl
+++ src/batch-ad/finitediff.jl
@@ -1,4 +1,4 @@
-struct BatchFiniteDiffJacobianPrep{SIG, X, Y, R, A, E} <: DI.JacobianPrep{SIG}
+struct BatchFiniteDiffJacobianPrep{SIG, X, Y, R, A, E, T} <: DI.JacobianPrep{SIG}
   _sig::Val{SIG}
   batchdim::Int
   x1::X
@@ -6,6 +6,7 @@ struct BatchFiniteDiffJacobianPrep{SIG, X, Y, R, A, E} <: DI.JacobianPrep{SIG}
   relstep::R
   absstep::A
   epsilon::E
+  contexts_cache::T
 end
 fdjtype(::AutoFiniteDiff{fdt, fdjt}) where {fdt, fdjt} = fdjt
 SparseMatrixColorings.sparsity_pattern(prep::BatchFiniteDiffJacobianPrep) = make_pattern(prep.x1, prep.y1, prep.batchdim)
@@ -41,7 +42,7 @@ function _prepare_batch_jacobian_aux(
   nx = size(x, otherdim)
   ny = size(y, otherdim)
   nlanes_prefd = size(x, batchdim)
-  nlanes_withfd = nlanes_prefd*(1 + (mode == Val{:central} ? 2*nx : nx))
+  nlanes_withfd = nlanes_prefd*(1 + nx*(mode == Val{:central} ? 2 : 1))
   x1 = similar(x, ntuple(i -> i == batchdim ? nlanes_withfd : nx, Val{2}()))
   y1 = similar(y, ntuple(i -> i == batchdim ? nlanes_withfd : ny, Val{2}()))
   epsilon = similar(x, ntuple(i -> i == batchdim ? nlanes_prefd : 1, Val{2}()))
@@ -57,35 +58,77 @@ function _prepare_batch_jacobian_aux(
     dense_ad(backend).absstep
   end
 
+  # Need to repeat AbstractArray contexts along batchdim:
+  # This is type unstable, but in prep step.
+  # Jacobian calculation uses type stable "iteration" over tuple
+  contexts_cache = map(contexts) do c
+    if DI.unwrap(c) isa AbstractArray # expand along batchdim
+      if size(DI.unwrap(c), batchdim) != nlanes_prefd
+        error("AbstractArray Contexts for AutoBatch{<:AutoFiniteDiff} must have a size along batchdim equal 
+               to the size along batchdim of the input array: received Context with size $(size(DI.unwrap(c), batchdim)) along batchdim.")
+      end
+      sb = size(DI.unwrap(c), batchdim)*(1 + size(DI.unwrap(c), otherdim)*(mode == Val{:central} ? 2 : 1))
+      DI.maker(c)(similar(DI.unwrap(c), ntuple(i -> i == batchdim ? sb : size(DI.unwrap(c), otherdim), Val{2}())))
+    else
+      c
+    end
+  end
+
    _sig = DI.signature(f_or_f!y..., backend, x, contexts...; strict)
-  return BatchFiniteDiffJacobianPrep(_sig, batchdim, x1, y1, relstep, absstep, epsilon)
+  return BatchFiniteDiffJacobianPrep(_sig, batchdim, x1, y1, relstep, absstep, epsilon, contexts_cache)
 end
 
 # Jacobian:
+@generated function set_contexts!(contexts_cache::T1, contexts::T2, batchdim) where {T1<:Tuple,T2<:Tuple}
+  N = length(T1.parameters)
+  @assert N == length(T2.parameters) "Length of contexts_cache and contexts tuple disagree."
+  exprs = [
+    :(_context_lower!(Base.getfield(contexts_cache, $i), Base.getfield(contexts, $i), batchdim)) 
+  for i in 1:N ]
+  return :(tuple($(exprs...)))
+end
+
+function _context_lower!(context_cache::Context, context::Context, batchdim)
+  return DI.maker(context_cache)(_set_context!(DI.unwrap(context_cache), DI.unwrap(context), batchdim))
+end
+
+_set_context!(context_cache_data::Number, context_data::Number, batchdim) = context_data
+function _set_context!(context_cache_data::AbstractArray, context_data::AbstractArray, batchdim)
+  chunksize = size(context_data, batchdim)
+  nx = size(context_data, mod(batchdim, 2) + 1)
+  reps = div(size(context_cache_data, batchdim), chunksize)
+  for i in 1:reps
+    context_cache_data[ntuple(j -> j == batchdim ? ((chunksize*(i-1)+1):(chunksize*i)) : (1:nx), Val{2}())...] .= context_data
+  end
+  return context_cache_data
+end
 
 function _value_and_jacobian_aux!(
     f_or_f!y::FY, jac, prep::BatchFiniteDiffJacobianPrep, backend::AutoBatch{<:AutoFiniteDiff}, x,
   ) where {FY}
-  _set_batch_fdj_input!(x, prep, backend)
+  @show prep.contexts_cache
+  set_batch_fdj_input!(x, prep, backend)
   if length(f_or_f!y) == 1
     f = f_or_f!y[1]
     prep.y1 .= f(prep.x1)
     # Allocate result:
     batchdim = prep.batchdim
     otherdim = mod(batchdim, 2) + 1
-    y = prep.y1[ntuple(i -> i == batchdim ? size(x, batchdim) : 1:size(y, otherdim), Val{2}())...]
+    y = prep.y1[ntuple(i -> i == batchdim ? size(x, batchdim) : 1:size(prep.y1, otherdim), Val{2}())...]
   else
     f! = f_or_f!y[1]
     y = f_or_f!y[2]
     f!(prep.y1, prep.x1)
     y[:] .= view(prep.y1, 1:length(y))
   end
-  _compute_batch_fdj_jac!(jac, prep, backend)
+  @show prep.x1
+  @show prep.y1
+  compute_batch_fdj_jac!(jac, prep, backend)
   return y, jac
 end
 
 # Initialize primal and tangents
-function _set_batch_fdj_input!(x, prep::BatchFiniteDiffJacobianPrep, backend::AutoBatch{<:AutoFiniteDiff})
+function set_batch_fdj_input!(x, prep::BatchFiniteDiffJacobianPrep, backend::AutoBatch{<:AutoFiniteDiff})
   x1 = prep.x1
   relstep = prep.relstep
   absstep = prep.absstep
@@ -133,7 +176,7 @@ function _set_batch_fdj_input!(x, prep::BatchFiniteDiffJacobianPrep, backend::Au
   return x1
 end
 
-function _compute_batch_fdj_jac!(jac::SparseArrays.AbstractSparseMatrixCSC, prep::BatchFiniteDiffJacobianPrep, backend::AutoBatch{<:AutoFiniteDiff})
+function compute_batch_fdj_jac!(jac::SparseArrays.AbstractSparseMatrixCSC, prep::BatchFiniteDiffJacobianPrep, backend::AutoBatch{<:AutoFiniteDiff})
   x1 = prep.x1
   y1 = prep.y1
 
@@ -142,7 +185,7 @@ function _compute_batch_fdj_jac!(jac::SparseArrays.AbstractSparseMatrixCSC, prep
   nx     = size(x1, otherdim)
   ny     = size(y1, otherdim)
   mode   = fdjtype(dense_ad(backend))
-  nlanes = div(size(x, batchdim), (mode == Val{:central} ? 2*nx + 1 : nx + 1))
+  nlanes = div(size(x1, batchdim), (mode == Val{:central} ? 2*nx + 1 : nx + 1))
 
   if batchdim == 1
     fwd  = reshape(view(y1, (nlanes+1):nlanes*(nx+1),            :), nx, nlanes, ny)
@@ -181,7 +224,8 @@ function DI.jacobian!(
     f::F, jac, prep::BatchFiniteDiffJacobianPrep, backend::AutoBatch{<:AutoFiniteDiff}, x, contexts::Vararg{DI.Context, C},
   ) where {F, C}
   DI.check_prep(f, prep, backend, x, contexts...)
-  fc = DI.fix_tail(f, map(DI.unwrap, contexts)...)
+  set_contexts!(prep.contexts_cache, contexts, backend.batchdim)
+  fc = DI.fix_tail(f, map(DI.unwrap, prep.contexts_cache)...)
   return _value_and_jacobian_aux!((fc,), jac, prep, backend, x)[2]
 end
 
@@ -189,7 +233,8 @@ function DI.jacobian(
     f::F, prep::BatchFiniteDiffJacobianPrep, backend::AutoBatch{<:AutoFiniteDiff}, x, contexts::Vararg{DI.Context, C},
   ) where {F, C}
   DI.check_prep(f, prep, backend, x, contexts...)
-  fc = DI.fix_tail(f, map(DI.unwrap, contexts)...)
+  set_contexts!(prep.contexts_cache, contexts, backend.batchdim)
+  fc = DI.fix_tail(f, map(DI.unwrap, prep.contexts_cache)...)
   jac = similar(sparsity_pattern(prep), eltype(prep.y1))
   return _value_and_jacobian_aux!((fc,), jac, prep, backend, x)[2]
 end
@@ -198,7 +243,8 @@ function DI.value_and_jacobian(
     f::F, prep::BatchFiniteDiffJacobianPrep, backend::AutoBatch{<:AutoFiniteDiff}, x, contexts::Vararg{DI.Context, C},
   ) where {F, C}
   DI.check_prep(f, prep, backend, x, contexts...)
-  fc = DI.fix_tail(f, map(DI.unwrap, contexts)...)
+  set_contexts!(prep.contexts_cache, contexts, backend.batchdim)
+  fc = DI.fix_tail(f, map(DI.unwrap, prep.contexts_cache)...)
   jac = similar(sparsity_pattern(prep), eltype(prep.y1))
   return _value_and_jacobian_aux!((fc,), jac, prep, backend, x)
 end
@@ -207,7 +253,8 @@ function DI.value_and_jacobian!(
     f::F, jac, prep::BatchFiniteDiffJacobianPrep, backend::AutoBatch{<:AutoFiniteDiff}, x, contexts::Vararg{DI.Context, C},
   ) where {F, C}
   DI.check_prep(f, prep, backend, x, contexts...)
-  fc = DI.fix_tail(f, map(DI.unwrap, contexts)...)
+  set_contexts!(prep.contexts_cache, contexts, backend.batchdim)
+  fc = DI.fix_tail(f, map(DI.unwrap, prep.contexts_cache)...)
   return _value_and_jacobian_aux!((fc,), jac, prep, backend, x)
 end
 
@@ -218,7 +265,8 @@ function DI.jacobian!(
     f!::F, y, jac, prep::BatchFiniteDiffJacobianPrep, backend::AutoBatch{<:AutoFiniteDiff}, x, contexts::Vararg{DI.Context, C},
   ) where {F, C}
   DI.check_prep(f!, y, prep, backend, x, contexts...)
-  fc! = DI.fix_tail(f!, map(DI.unwrap, contexts)...)
+  set_contexts!(prep.contexts_cache, contexts, backend.batchdim)
+  fc! = DI.fix_tail(f!, map(DI.unwrap, prep.contexts_cache)...)
   return _value_and_jacobian_aux!((fc!, y), jac, prep, backend, x)
 end
 
@@ -226,7 +274,8 @@ function DI.jacobian(
     f!::F, y, prep::BatchFiniteDiffJacobianPrep, backend::AutoBatch{<:AutoFiniteDiff}, x, contexts::Vararg{DI.Context, C},
   ) where {F, C}
   DI.check_prep(f!, y, prep, backend, x, contexts...)
-  fc! = DI.fix_tail(f!, map(DI.unwrap, contexts)...)
+  set_contexts!(prep.contexts_cache, contexts, backend.batchdim)
+  fc! = DI.fix_tail(f!, map(DI.unwrap, prep.contexts_cache)...)
   jac = similar(sparsity_pattern(prep), eltype(prep.y1))
   return _value_and_jacobian_aux!((fc!, y), jac, prep, backend, x)
 end
@@ -235,7 +284,8 @@ function DI.value_and_jacobian(
     f!::F, y, prep::BatchFiniteDiffJacobianPrep, backend::AutoBatch{<:AutoFiniteDiff}, x, contexts::Vararg{DI.Context, C},
   ) where {F, C}
   DI.check_prep(f!, y, prep, backend, x, contexts...)
-  fc! = DI.fix_tail(f!, map(DI.unwrap, contexts)...)
+  set_contexts!(prep.contexts_cache, contexts, backend.batchdim)
+  fc! = DI.fix_tail(f!, map(DI.unwrap, prep.contexts_cache)...)
   jac = similar(sparsity_pattern(prep), eltype(prep.y1))
   return _value_and_jacobian_aux!((fc!, y), jac, prep, backend, x)
 end
@@ -244,6 +294,7 @@ function DI.value_and_jacobian!(
     f!::F, y, jac, prep::BatchFiniteDiffJacobianPrep, backend::AutoBatch{<:AutoFiniteDiff}, x, contexts::Vararg{DI.Context, C},
   ) where {F, C}
   DI.check_prep(f!, y, prep, backend, x, contexts...)
-  fc! = DI.fix_tail(f!, map(DI.unwrap, contexts)...)
+  set_contexts!(prep.contexts_cache, contexts, backend.batchdim)
+  fc! = DI.fix_tail(f!, map(DI.unwrap, prep.contexts_cache)...)
   return _value_and_jacobian_aux!((fc!, y), jac, prep, backend, x)
 end
